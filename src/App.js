b4dbdbf33a2022559eb79dd7676c2e8b23576d2a
Michael Merani <merani491@outlook.com>
2020-01-29 16:57:40 -0800

    Added loading spinner

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Container from 'react-bootstrap/Container';
-import Table from 'react-bootstrap/Table'
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
+import Table from 'react-bootstrap/Table';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Spinner from 'react-bootstrap/Spinner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -16,7 +17,7 @@ class App extends Component {
 
     this.state = {
       dropDownValue: "Linear Regression",
-      isLoading: false,
+      isLoading: true,
       formData:{
         model: 0
       },
@@ -57,9 +58,7 @@ class App extends Component {
       console.log(this.state.result);   
   }
   render() {
-    if(!this.state.result){
-      return <div />
-    }
+    
     return (
       <Container>
         <div>
@@ -69,55 +68,67 @@ class App extends Component {
           <div className="dropdown">
             <Dropdown>
 
-              <DropdownButton id="dropdown-basic-button" title={this.state.dropDownValue} className="format">
+              <DropdownButton id="dropdown-basic-button" title={this.state.dropDownValue} className="format" disabled = {this.state.isLoading}>
                 {/* <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent)}>Linear Regression</div></Dropdown.Item> */}
-                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent,0)}>Linear Regression</div></Dropdown.Item>
-                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent,1)}>Gradient Descent</div></Dropdown.Item>
-                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent,2)}>Ridge Regression</div></Dropdown.Item>
-                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent,3)}>Lasso Regression</div></Dropdown.Item>
-                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent,4)}>Elastic Net</div></Dropdown.Item>
+                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent, 0)}>Linear Regression</div></Dropdown.Item>
+                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent, 1)}>Gradient Descent</div></Dropdown.Item>
+                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent, 2)}>Ridge Regression</div></Dropdown.Item>
+                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent, 3)}>Lasso Regression</div></Dropdown.Item>
+                <Dropdown.Item as="button"><div onClick={(e) => this.handlePredictClick(e.target.textContent, 4)}>Elastic Net</div></Dropdown.Item>
               </DropdownButton>
             </Dropdown>
           </div>
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Player Name</th>
-                <th>Team</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>{this.state.result[0].Player}</td>
-                <td>{this.state.result[0].Team}</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>{this.state.result[1].Player}</td>
-                <td>{this.state.result[1].Team}</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>{this.state.result[2].Player}</td>
-                <td>{this.state.result[2].Team}</td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>{this.state.result[3].Player}</td>
-                <td>{this.state.result[3].Team}</td>
-              </tr>
-              <tr>
-                <td>5</td>
-                <td>{this.state.result[4].Player}</td>
-                <td>{this.state.result[4].Team}</td>
-              </tr>
-            </tbody>
-          </Table>
+          {this.state.result === "" ?
+            (<div style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+              }}
+              >
+              <Spinner animation="border" variant="primary" role="status">
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+              </div>) :
+            (<Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Player Name</th>
+              <th>Team</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>{this.state.result[0].Player}</td>
+              <td>{this.state.result[0].Team}</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>{this.state.result[1].Player}</td>
+              <td>{this.state.result[1].Team}</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>{this.state.result[2].Player}</td>
+              <td>{this.state.result[2].Team}</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>{this.state.result[3].Player}</td>
+              <td>{this.state.result[3].Team}</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>{this.state.result[4].Player}</td>
+              <td>{this.state.result[4].Team}</td>
+            </tr>
+          </tbody>
+        </Table>)
+      }
 
         </div>
-      </Container>
+      </Container >
     );
   }
 }
